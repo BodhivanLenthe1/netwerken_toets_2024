f992c640ef782768b7448109c719f9c9178f3f33
BodhivanLenthe1 <131263370+BodhivanLenthe1@users.noreply.github.com>
2024-04-16 10:05:41 +0200

    Update helpdesk.php

--- contact/helpdesk.php
+++ contact/helpdesk.php
@@ -80,7 +80,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     if(empty($nameErr) && empty($emailErr) && empty($messageErr)){
         // Recipient email address
         // this should be <username>@username.gc-webhosting.nl
-        $to = 'someone@address.gc-webhosting.nl';
+        $to = 'net24blenthe@net24blenthe.gc-webhosting.nl
+';
         
         // Create email headers
         $headers = 'From: '. $email . "\r\n" .
